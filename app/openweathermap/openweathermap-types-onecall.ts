237fcd155a16f33b59490eba6bcaba50f1c1b614
jaredkc <jaredkc@gmail.com>
2023-10-25 20:49:40 -0600

    OneCall optional rain and snow data

--- app/openweathermap/openweathermap-types-onecall.ts
+++ app/openweathermap/openweathermap-types-onecall.ts
@@ -2,99 +2,103 @@
 // Generated with https://transform.tools/json-to-typescript
 
 export interface Forecast {
-  lat: number
-  lon: number
-  timezone: string
-  timezone_offset: number
-  current: Current
-  hourly: Hourly[]
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  current: Current;
+  hourly: Hourly[];
   // Exlucding for now
   // minutely: Minutely[]
   // daily: Daily[]
 }
 
 export interface Current {
-  dt: number
-  sunrise: number
-  sunset: number
-  temp: number
-  feels_like: number
-  pressure: number
-  humidity: number
-  dew_point: number
-  uvi: number
-  clouds: number
-  visibility: number
-  wind_speed: number
-  wind_deg: number
-  wind_gust: number
-  weather: Weather[]
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust?: number;
+  weather: Weather[];
+  rain?: { "1h": number };
+  snow?: { "1h": number };
 }
 
 export interface Weather {
-  id: number
-  main: string
-  description: string
-  icon: string
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
 }
 
 export interface Minutely {
-  dt: number
-  precipitation: number
+  dt: number;
+  precipitation: number;
 }
 
 export interface Hourly {
-  dt: number
-  temp: number
-  feels_like: number
-  pressure: number
-  humidity: number
-  dew_point: number
-  uvi: number
-  clouds: number
-  visibility: number
-  wind_speed: number
-  wind_deg: number
-  wind_gust: number
-  weather: Weather[]
-  pop: number
+  dt: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust?: number;
+  weather: Weather[];
+  pop: number;
+  rain?: { "1h": number };
+  snow?: { "1h": number };
 }
 
 export interface Daily {
-  dt: number
-  sunrise: number
-  sunset: number
-  moonrise: number
-  moonset: number
-  moon_phase: number
-  summary: string
-  temp: Temp
-  feels_like: FeelsLike
-  pressure: number
-  humidity: number
-  dew_point: number
-  wind_speed: number
-  wind_deg: number
-  wind_gust: number
-  weather: Weather[]
-  clouds: number
-  pop: number
-  rain: number
-  uvi: number
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  summary: string;
+  temp: Temp;
+  feels_like: FeelsLike;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  rain: number;
+  uvi: number;
 }
 
 export interface Temp {
-  day: number
-  min: number
-  max: number
-  night: number
-  eve: number
-  morn: number
+  day: number;
+  min: number;
+  max: number;
+  night: number;
+  eve: number;
+  morn: number;
 }
 
 export interface FeelsLike {
-  day: number
-  night: number
-  eve: number
-  morn: number
+  day: number;
+  night: number;
+  eve: number;
+  morn: number;
 }
