8762f3a017ea28d8813c9d634958c5bb6023ad0d
jaredkc <jaredkc@gmail.com>
2023-11-19 15:47:43 -0700

    use images the Remix/React way 🙄

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import bg1 from "./assets/bg-gradient-1.png";
+import bg2 from "./assets/bg-gradient-2.png";
 
 import { getUser } from "~/session.server";
 import twStyles from "~/styles/tailwind.css";
@@ -38,14 +40,14 @@ export default function App() {
         <Outlet />
         <div className="overflow-hidden">
           <img
-            src="/_static/bg-gradient-1.png"
+            src={bg1}
             className="fixed top-0 -translate-x-1/2 left-1/2 -z-10"
             width="800"
             height="380"
             alt=""
           />
           <img
-            src="/_static/bg-gradient-2.png"
+            src={bg2}
             className="fixed -translate-x-1/3 top-16 left-1/3 -z-10"
             width="625"
             height="565"
