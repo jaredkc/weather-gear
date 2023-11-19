d20af25a5f06802f48964217de2bb4e6b5ed0b25
jaredkc <jaredkc@gmail.com>
2023-11-19 14:27:26 -0700

    add gradient background

--- app/root.tsx
+++ app/root.tsx
@@ -34,8 +34,24 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="h-full text-slate-300 bg-gradient-to-b from-slate-800 to-slate-900">
+      <body className="h-full text-slate-300">
         <Outlet />
+        <div className="overflow-hidden">
+          <img
+            src="/_static/bg-gradient-1.png"
+            className="fixed top-0 -translate-x-1/2 left-1/2 -z-10"
+            width="800"
+            height="380"
+            alt=""
+          />
+          <img
+            src="/_static/bg-gradient-2.png"
+            className="fixed -translate-x-1/3 top-16 left-1/3 -z-10"
+            width="625"
+            height="565"
+            alt=""
+          />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
