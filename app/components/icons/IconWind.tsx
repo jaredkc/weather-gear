d5c8a0981e1221c8799fce03e04a518f9946ba5b
jaredkc <jaredkc@gmail.com>
2023-11-19 14:57:36 -0700

    Test for minified React error fix

--- app/components/icons/IconWind.tsx
+++ app/components/icons/IconWind.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-
-interface Props extends React.SVGProps<SVGSVGElement> {}
-
-export function IconWind(props: Props) {
+export function IconWind() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height="20"
       viewBox="0 -960 960 960"
       width="20"
-      {...props}
     >
       <path
         fill="currentColor"
