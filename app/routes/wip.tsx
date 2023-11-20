fbe42c94b918fe8d2e0ef5198582363352ca3622
jaredkc <jared@clivecoffee.com>
2023-11-20 10:30:44 -0700

    template for UI progress

---
+++ app/routes/wip.tsx
@@ -0,0 +1,72 @@
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import clsx from "clsx";
+import { useState } from "react";
+import { AppFrame } from "~/components/AppFrame";
+import { Card } from "~/components/Card";
+import { GearList } from "~/components/GearList";
+import { HourlyList } from "~/components/HourlyList";
+import { LocationCard } from "~/components/LocationCard";
+import { cyclingGear, gearForTemp } from "~/gear";
+import { forecastToUserLocation } from "~/models/user-location.server";
+import { sampleGeocoding } from "~/openweathermap/data/sample-geocoding";
+import { sampleOneCall } from "~/openweathermap/data/sample-onecall";
+
+export const meta: MetaFunction = () => [
+  { title: "Work in progress screen" },
+];
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const forecast = sampleOneCall;
+  const location = sampleGeocoding;
+
+  const gear = gearForTemp(cyclingGear, forecast.hourly[0].feels_like);
+  const userLocation = forecastToUserLocation(location[0].name, forecast);
+
+  return json({ forecast, gear, userLocation });
+};
+
+export default function CyclingIndex() {
+  const { forecast, gear, userLocation } = useLoaderData<typeof loader>();
+
+  const [gearList, setGearList] = useState(gear);
+  const [activeHour, setActiveHour] = useState(0);
+
+  const handleActiveHour = (temp: number, index: number) => {
+    setActiveHour(index);
+    setGearList(gearForTemp(cyclingGear, temp));
+  };
+
+  return (
+    <AppFrame>
+      <div className="flex flex-col gap-4">
+        <LocationCard location={userLocation} />
+
+        <Card>
+          <div className="flex overflow-x-auto snap-x">
+            {forecast.hourly.map((hour, index) => (
+              <button
+                key={index}
+                onClick={() => handleActiveHour(hour.temp, index)}
+                className={clsx(
+                  "flex-none cursor-pointer snap-start relative border-white rounded-lg border-4",
+                  index === activeHour && "bg-slate-200 border-slate-300",
+                )}
+              >
+                <HourlyList hour={hour} timezone={forecast.timezone} />
+              </button>
+            ))}
+          </div>
+        </Card>
+
+        <Card>
+          <GearList gear={gearList} />
+        </Card>
+      </div>
+    </AppFrame>
+  );
+}
