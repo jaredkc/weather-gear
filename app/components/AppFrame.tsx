3ad1a47f324746e525034fba243fd2faa5c4eae9
jaredkc <jaredkc@gmail.com>
2023-11-20 18:10:55 -0700

    style progress for details view

--- app/components/AppFrame.tsx
+++ app/components/AppFrame.tsx
@@ -14,13 +14,14 @@ export const AppFrame = ({ children, navItems, showBack }: Props) => {
       <div className="flex flex-col max-w-sm mx-auto min-h-[100svh]">
         <header className="flex items-center justify-between h-20 text-white">
           {showBack ? (
-            <Link to="/" className="flex items-center gap-1">
+            <Link
+              to="/"
+              className="flex items-center gap-1 text-slate-300 hover:text-white"
+            >
               <span className="-ml-2 rotate-180 text-cyan-400">
                 <IconChevron />
               </span>
-              <span className="text-xs tracking-widest uppercase text-slate-300">
-                back
-              </span>
+              <span className="text-xs tracking-widest uppercase">back</span>
             </Link>
           ) : (
             <WeatherGearLogo />
