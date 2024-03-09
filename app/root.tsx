e6cade265b75072cae795efe05327dceacea0976
jaredkc <jaredkc@gmail.com>
2024-03-09 13:34:50 -0700

    default error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import bg1 from "./assets/bg-gradient-1.png";
 import bg2 from "./assets/bg-gradient-2.png";
@@ -60,6 +61,43 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <html className="h-full bg-slate-900">
+      <head>
+        <title>Oh snap!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full text-slate-300">
+        <div className="flex flex-col mx-auto min-h-[100svh] text-center justify-center gap-2">
+          <h1 className="text-3xl font-light">Oh snap!</h1>
+          <p>Something went wrong.</p>
+        </div>
+        <div className="overflow-hidden">
+          <img
+            src={bg1}
+            className="fixed top-0 -translate-x-1/2 left-1/2 -z-10"
+            width="800"
+            height="380"
+            alt=""
+          />
+          <img
+            src={bg2}
+            className="fixed -translate-x-1/3 top-16 left-1/3 -z-10"
+            width="625"
+            height="565"
+            alt=""
+          />
+        </div>
+        <Scripts />
       </body>
     </html>
   );
