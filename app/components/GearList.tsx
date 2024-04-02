207a309101a79667e3e711d9c753b86320f03d80
jaredkc <jared@clivecoffee.com>
2024-04-02 14:22:49 -0600

    show temp range for gear

--- app/components/GearList.tsx
+++ app/components/GearList.tsx
@@ -8,9 +8,13 @@ export const GearList = ({ gear }: Props) => {
   return (
     <ul className="text-sm text-left divide-y divide-slate-800">
       {gear.map((item) => (
-        <li key={item.id} className="px-4 py-3">
+        <li key={item.id} className="px-4 py-3 flex justify-between">
           {item.name}
+          <span className="opacity-50">
+            {item.min_temp}°-{item.max_temp}°
+          </span>
         </li>
+
       ))}
     </ul>
   );
