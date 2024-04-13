14c8b6f551e4099c97a6b7ace82174a382993799
jaredkc <jaredkc@gmail.com>
2024-04-12 22:14:43 -0600

    Use webp background images

--- app/root.tsx
+++ app/root.tsx
@@ -84,14 +84,14 @@ export default function App() {
         <Outlet />
         <div className="overflow-hidden">
           <img
-            src="/_static/bg-gradient-1.png"
+            src="/_static/bg-gradient-1.webp"
             className="fixed top-0 -translate-x-1/2 left-1/2 -z-10"
             width="800"
             height="380"
             alt=""
           />
           <img
-            src="/_static/bg-gradient-2.png"
+            src="/_static/bg-gradient-2.webp"
             className="fixed -translate-x-1/3 top-16 left-1/3 -z-10"
             width="625"
             height="565"
@@ -133,14 +133,14 @@ export function ErrorBoundary() {
         </div>
         <div className="overflow-hidden">
           <img
-            src="/_static/bg-gradient-1.png"
+            src="/_static/bg-gradient-1.webp"
             className="fixed top-0 -translate-x-1/2 left-1/2 -z-10"
             width="800"
             height="380"
             alt=""
           />
           <img
-            src="/_static/bg-gradient-2.png"
+            src="/_static/bg-gradient-2.webp"
             className="fixed -translate-x-1/3 top-16 left-1/3 -z-10"
             width="625"
             height="565"
